f5af6ba2e69c8ed9ded1535f5ecef0963f2a3927
mslmyrtd <mslmyrtd@gmail.com>
2022-01-01 20:16:09 +0300

    debug solution

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,7 @@
-import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import { AuthContext } from "../context/AuthContext";
-import { signOut } from "firebase/auth";
-import { auth } from "../auth/firebase-config";
 
 const Navbar = () => {
   const navigate = useNavigate();
-  const { currentUser } = useContext(AuthContext);
-
-  const signOutFunc = async () => {
-    await signOut(auth);
-  };
 
   return (
     <div>
@@ -20,34 +11,13 @@ const Navbar = () => {
             React Movie App
           </a>
           <div className="buttons">
-            {currentUser ? (
-              <h3>{currentUser.displayName}</h3>
-            ) : (
-              <button
-                type="button"
-                className="ms-2 btn btn-outline-light"
-                onClick={() => navigate("/login")}
-              >
-                Login
-              </button>
-            )}
-            {currentUser ? (
-              <button
-                type="button"
-                className="ms-2 btn btn-outline-light"
-                onClick={() => signOutFunc()}
-              >
-                Logout
-              </button>
-            ) : (
-              <button
-                type="button"
-                className="ms-2 btn btn-outline-light"
-                onClick={() => navigate("/register")}
-              >
-                Register
-              </button>
-            )}
+            <button
+              type="button"
+              className="ms-2 btn btn-outline-light"
+              onClick={() => navigate("/login")}
+            >
+              Login
+            </button>
           </div>
         </div>
       </nav>
